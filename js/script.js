34a7b82bea474955083b104926f7137abc50a59a
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-08 14:13:15 +0100

    print on page with for cycle

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 const element = document.getElementById('target');
 
 const shoppingList = ['Uova', 'Farina', 'Latte', 'Biscotti', 'Nutella'];
+
+/*
+let listItems = '<ul>';
+
+for (let i = 0; i < shoppingList.length; i++) {
+    listItems += `<li>${shoppingList[i]}</li>`;
+}
+
+listItems += '</ul>';
+
+console.log(listItems);
+
+element.innerHTML = listItems;
+*/
